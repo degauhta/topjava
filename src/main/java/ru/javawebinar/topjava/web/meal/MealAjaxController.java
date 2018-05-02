a50a7e09cce3b3c169c762be4fc5a611f21794b9
Denis Gagarsky <dega.uhta@gmail.com>
2018-05-02 05:23:27 +0300

    8-10 HW08

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -0,0 +1,43 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealWithExceed;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ajax/meals/")
+public class MealAjaxController extends AbstractMealController {
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealWithExceed> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @PostMapping
+    public void createOrUpdate(@RequestParam("id") Integer id,
+                               @RequestParam("dateTime") String dateTime,
+                               @RequestParam("description") String description,
+                               @RequestParam("calories") Integer calories) {
+        Meal meal = new Meal(id, LocalDateTime.parse(dateTime), description, calories);
+        if (meal.isNew()) {
+            super.create(meal);
+        }
+    }
+}
